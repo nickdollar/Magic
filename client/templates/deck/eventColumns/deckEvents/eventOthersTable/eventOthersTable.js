ad3d71d958529468723fd46a909ff9a7ccecc486
Nicholas Homolka <ivelacc@gmail.com>
2016-11-06 16:04:28 -0700

    Site terminado

--- client/templates/deck/eventColumns/deckEvents/eventOthersTable/eventOthersTable.js
+++
@@ -1,5 +0,0 @@
-Template.eventOthersTable.helpers({
-   daily : function(){
-
-   }
-});
---
+++ client/templates/event/EventsTable.js/EventsTables.js
@@ -0,0 +1,16 @@
+Template.EventsTables.helpers({
+    mtgoEvents : function(){
+
+    }
+});
+
+Template.EventsTables.events({
+    //"click .clickable-row" : function(evt, template){
+    //    window.document.location = $(evt.target).data("href");
+    //}
+});
+
+
+Template.EventsTables.onRendered(function(){
+
+});
